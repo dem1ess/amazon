4e4c1fab395cb94577895fce0c634406a406d618
Stanislav Kharchenko <stanislav_kharchenko@outlook.com>
2024-01-05 21:18:37 +0200

    update new resources and Prisma

--- back-end-amazon/src/main.ts
+++ back-end-amazon/src/main.ts
@@ -1,8 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule)
+	const port = process.env.APP_PORT || 3001
+	app.setGlobalPrefix('api')
+	app.enableCors()
+	await app.listen(port)
+	console.log('Start at port: ', port)
 }
-bootstrap();
+bootstrap()
