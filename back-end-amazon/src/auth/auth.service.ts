ff80f6cb5cae608f4d4974b5c34149f963d459e3
Stanislav Kharchenko <stanislav_kharchenko@outlook.com>
2024-01-06 18:04:53 +0200

    update auth service add prisma

--- back-end-amazon/src/auth/auth.service.ts
+++ back-end-amazon/src/auth/auth.service.ts
@@ -1,10 +1,37 @@
-import { Injectable } from '@nestjs/common'
+import { faker } from '@faker-js/faker'
+import { BadRequestException, Injectable } from '@nestjs/common'
+import { hash } from 'argon2'
+import { PrismaService } from 'src/prisma.service'
+import { AuthDto } from './auth.dto'
 
 @Injectable()
 export class AuthService {
-	register() {
-		return {
-			name: 'Maxim'
-		}
+	constructor(private prisma: PrismaService) {}
+
+	async register(dto: AuthDto) {
+		const oldUser = await this.prisma.user.findUnique({
+			where: {
+				email: dto.email
+			}
+		})
+
+		if (oldUser) throw new BadRequestException('User already exists')
+
+		const user = await this.prisma.user.create({
+			data: {
+				email: dto.email,
+				name: faker.name.firstName(),
+				avatarPath: faker.image.avatar(),
+				phone: faker.phone.number('+38 (###)###-##-##'),
+				password: await hash(dto.password)
+			}
+		})
+		return user
+	}
+
+	private async issueTokens(userId: number) {
+		const data = { id: userId }
+
+		// const accessToken = this.jwtService.sign(data)
 	}
 }
